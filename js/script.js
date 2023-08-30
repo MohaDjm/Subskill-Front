c6f272f0144554c2d78e9f4995bdd2a628214420
Mohamed <mohamedjoumoi@gmail.com>
2023-08-30 04:15:03 +0200

    feat: section equipe

--- js/script.js
+++ js/script.js
@@ -87,6 +87,8 @@ document.querySelector('.navbar__menus').addEventListener('mouseleave', function
 // });
 
 
+// Slider 
+
 document.addEventListener("DOMContentLoaded", () => {
     const slider = document.querySelector(".slider");
     const slides = document.querySelectorAll(".slide");
@@ -135,4 +137,27 @@ document.addEventListener("DOMContentLoaded", () => {
         next.disabled = index === maxIndex;
     }
     document.documentElement.style.setProperty('--thumb-width', `${percentage}%`);
+});
+
+
+// Pop in team members
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Trouve toutes les cartes avec un popup
+    const cardsWithPopup = document.querySelectorAll('.equipe__cards.has-popup');
+
+    cardsWithPopup.forEach(card => {
+        // Quand on clique sur la carte
+        card.querySelector('a').addEventListener('click', function(e) {
+            e.preventDefault();
+            // On affiche le popup associé à cette carte
+            card.querySelector('.popup').style.display = 'block';
+        });
+
+        // Quand on clique sur le bouton de fermeture du popup
+        card.querySelector('.close-popup').addEventListener('click', function() {
+            // On ferme le popup
+            card.querySelector('.popup').style.display = 'none';
+        });
+    });
 });
